970ce0782cb628eaa10a9c4c1f576f6b31bf3ed8
Raghavendra Mirajkar <raghavendramirajkar@Raghavendras-MacBook-Air.local>
2024-03-10 07:08:46 +0530

    Added the BMI calculations

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var heightData: UISlider!
+    @IBOutlet weak var weightData: UISlider!
+    
     @IBOutlet weak var weight: UILabel!
     @IBOutlet weak var height: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -26,6 +31,12 @@ class ViewController: UIViewController {
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        let weight = weightData.value/2
+        let height = heightData.value/2
+        var BMI = (weight) / (pow(height, 2))
+        print(weight)
+        print(height)
+        print(BMI)
     }
     
 
